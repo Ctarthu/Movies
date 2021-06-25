4e2931ff5b245248d9476e832373a5b89459515f
Saba Abramishvili <grisha.badri@gmail.com>
2021-06-26 00:13:30 +0400

    movie slider

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 
 
+
+const rightarrow=document.querySelector('.right-arrow')
+const leftarrow=document.querySelector('.left-arrow')
+const movielist=document.querySelector('.movie-list')
+const movies=document.querySelectorAll('.movie-list-item')
+const right=movielist.getBoundingClientRect().right
+const left=movielist.getBoundingClientRect().left
+const mid=-(Math.round(movies.length/2)-2)*450  
+console.log(mid);
+movielist.style.transform = `translateX(${mid}px)`;
+
 function test(){
     console.log(movielist.getBoundingClientRect().right)
     console.log(movielist.getBoundingClientRect().left);
@@ -8,6 +19,22 @@ function test(){
 test()
 
 
+rightarrow.addEventListener("click",()=>{
+    if(movielist.getBoundingClientRect().right>1858){
+    movielist.style.transform = 
+    `translateX(${movielist.getBoundingClientRect().right - right -450}px)`;
+    test()
+    }
+    })
+    
+leftarrow.addEventListener("click",()=>{
+    if(movielist.getBoundingClientRect().left<58){
+        movielist.style.transform = 
+        `translateX(${movielist.getBoundingClientRect().left - left +450}px)`;
+        test()
+    }
+        
+    })
 
 
 function openFeatured(){
