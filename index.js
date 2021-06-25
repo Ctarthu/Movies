d59cb57fc443f15b4b09c91d391b9344290e1766
Saba Abramishvili <grisha.badri@gmail.com>
2021-06-25 23:45:13 +0400

    Movies

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function test(){
+    console.log(movielist.getBoundingClientRect().right)
+    console.log(movielist.getBoundingClientRect().left);
+}
+
+test()
+
+
+
+
+function openFeatured(){
+    window.open("starwars.html", "_blank");
+}
+
+
